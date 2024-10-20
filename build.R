e8661f3f53ee2c13e88a1f030479cbd25d1b53ed
Tingjie Guo <t.guo@lacdr.leidenuniv.nl>
2024-10-20 22:00:59 +0200

    Refactor build.R to insert a container div within the body tag

--- build.R
+++ build.R
@@ -1,11 +1,11 @@
 library(stringr)
 
 # define the code to be added
-html_code <- "
+html_code <- '
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="stylesheet" href="style.css" />
-"
+'
 
 # define the function to insert the code
 insert_html_and_div <- function(file_path, html_code) {
@@ -19,7 +19,7 @@ insert_html_and_div <- function(file_path, html_code) {
   
   # insert <div> to include everything within <body>, case-insensitive
   if (str_detect(content, regex("<body([^>]*)>", ignore_case = TRUE)) && str_detect(content, regex("</body>", ignore_case = TRUE))) {
-    content <- str_replace(content, regex("<body([^>]*)>", ignore_case = TRUE), "<body\\1><div class="container">")
+    content <- str_replace(content, regex("<body([^>]*)>", ignore_case = TRUE), '<body\\1><div class="container">')
     content <- str_replace(content, regex("</body>", ignore_case = TRUE), "</div></body>")
   }
   
